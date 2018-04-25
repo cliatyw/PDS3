b97975f8081d33ac8bdd37a71fa1c9b07ce95bec
smart-202-16 <smart-202-16@smart-202-16-PC>
2018-04-25 09:08:50 +0900

    [김도희]ResumeFileDao 추가

---
+++ src/main/java/com/test/pds/resume/service/ResumeFileDao.java
@@ -0,0 +1,20 @@
+package com.test.pds.resume.service;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ResumeFileDao {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	final String NS = "com.test.pds.notice.service.ResumeFileMapper.";
+	
+	/*ResumeFile 입력*/
+	public int insertResumeFile(ResumeFile resumeFile) {
+		int row = sqlSession.insert(NS+"insertResumeFile", resumeFile);
+		return row;
+	}
+	
+}
